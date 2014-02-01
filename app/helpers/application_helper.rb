db0f3f2a83223296b8207ee0816df28d9c07c94c
lijiashen <sysulijs@gmail.com>
2014-02-01 23:32:28 +0800

    modify layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,10 @@
 module ApplicationHelper
   def full_title(page_title)
-<<<<<<< HEAD
   	base_title = "Ruby on Rails Tutorial Sample App"
   	if page_title.empty?
   	  base_title
   	else
   	  "#{base_title} | #{page_title}"
   	end
-=======
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
->>>>>>> filling-in-layout
   end
 end
